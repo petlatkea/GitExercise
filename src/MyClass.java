9239a3869fdeaddbbde43bb196b0e7ac405cc6cc
Peter Lind <petl@kea.dk>
2022-02-25 10:28:58 +0100

    add askAboutAge

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class MyClass {
+
+    public int askAboutAge() {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        return age;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Det virker!");
         System.out.println("Det virker stadig");
